dfdf1c356cc864985ee32f8387992aa5b1d5eb8d
nzilla <victorbliss044@gmail.com>
2024-09-21 00:21:20 +0100

    prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+
+        string firstName = Console.ReadLine();
+
+        Console.Write("what is your last name? ");
+
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"your name is {lastName}, {firstName} {lastName}.");
+
     }
 }
